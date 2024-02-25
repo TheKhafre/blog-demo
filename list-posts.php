bd8a22f38e3e976623dc77bfeeacd45b5acca1c9
Oluwatobiloba Ezekiel <mroluwatobilobaezekiel@gmail.com>
2024-02-25 13:07:45 +0100

    handle special error like user deleting post

--- list-posts.php
+++ list-posts.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'lib/common.php';
+require_once 'lib/list-posts.php';
 
 session_start();
 
@@ -8,6 +9,25 @@ if (!isLoggedIn())
 {
     redirectAndExit('index.php');
 }
+
+if ($_POST)
+{
+    $deleteResponse = $_POST['delete-post'];
+    if ($deleteResponse)
+    {
+        $keys = array_keys($deleteResponse);
+        $deletePostId = $keys[0];
+        if ($deletePostId)
+        {
+            deletePost(getPDO(), $deletePostId);
+            redirectAndExit('list-posts.php');
+        }
+    }
+}
+
+// Connect to the database, run a query
+$pdo = getPDO();
+$posts = getAllPosts($pdo);
 
 ?>
 <!DOCTYPE html>
@@ -24,45 +44,26 @@ if (!isLoggedIn())
         <form method="post">
             <table id="post-list">
                 <tbody>
-                    <tr>
-                        <td>Title of the first post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=1">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[1]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Title of the second post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=2">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[2]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Title of the third post</td>
-                        <td>
-                            <a href="edit-post.php?post_id=3">Edit</a>
-                        </td>
-                        <td>
-                            <input
-                                type="submit"
-                                name="post[3]"
-                                value="Delete"
-                            />
-                        </td>
-                    </tr>
+                    <?php foreach ($posts as $post): ?>
+                        <tr>
+                            <td>
+                                <?php echo htmlEscape($post['title']) ?>
+                            </td>
+                            <td>
+                                <?php echo convertSqlDate($post['created_at']) ?>
+                            </td>
+                            <td>
+                                <a href="edit-post.php?post_id=<?php echo $post['id']?>">Edit</a>
+                            </td>
+                            <td>
+                                <input
+                                    type="submit"
+                                    name="delete-post[<?php echo $post['id']?>]"
+                                    value="Delete"
+                                />
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
                 </tbody>
             </table>
         </form>
